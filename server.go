58b707fe673b4302f2620998350ed7f3e37386a3
Justin Etzine <justetz@gmail.com>
2016-04-11 11:18:17 -0400

    Implemented schema values for Documents and Categories

--- server.go
+++ server.go
@@ -8,9 +8,33 @@ import (
 	// _ "github.com/lib/pq"
 )
 
+type Category struct {
+    Id int
+    Name string
+    Description string
+    IsPrivate bool
+    IsWritable bool
+    IsFeatured bool
+    ParentId int
+    GroupId int
+    CreatedAt string
+    UpdatedAt string
+}
+
+type Document struct {
+    Id int
+    Title string
+    Description string
+    IsPrivate bool
+    IsWriteable bool
+    CategoryId int
+    UserId int
+    CreatedAt string
+    UpdatedAt string
+}
+
 type Page struct {
-    Categories []string
-    Files []string
+    Categories []Category
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -22,8 +46,19 @@ func index(w http.ResponseWriter, r *http.Request) {
     }
 
     p := Page{
-        Categories: []string{"Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category"},
-        Files: []string{"File", "File", "File", "File", "File"},
+        Categories: []Category{
+            Category{Id:  1, Name:"Test"},
+            Category{Id:  2, Name:"Test"},
+            Category{Id:  3, Name:"Test"},
+            Category{Id:  4, Name:"Test"},
+            Category{Id:  5, Name:"Test"},
+            Category{Id:  6, Name:"Test"},
+            Category{Id:  7, Name:"Test"},
+            Category{Id:  8, Name:"Test"},
+            Category{Id:  9, Name:"Test"},
+            Category{Id: 10, Name:"Test"},
+            Category{Id: 11, Name:"Test"},
+        },
     }
     t.Execute(w, p)
 }
