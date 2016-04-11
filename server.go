8cd759ed81b1c16fcf04044d60364fb716b6c426
Justin Etzine <justetz@gmail.com>
2016-04-11 00:58:29 -0400

    It's the start of something new (https://www.youtube.com/watch?v=I6EOUaWscrE)

---
+++ server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "net/http"
+    "log"
+    "html/template"
+    // "database/sql"
+	// _ "github.com/lib/pq"
+)
+
+type Page struct {
+    Test string
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+    // fmt.Fprintf(w, "hello world")
+    t, err := template.ParseFiles("templates/index.html")
+    if err != nil {
+        // TODO: 500 handler
+        log.Fatal(err)
+    }
+
+    p := Page{Test: "template test"}
+    t.Execute(w, p)
+}
+
+func main() {
+    // _, err := sql.Open("postgres", "user=root dbname=flagship")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+    http.HandleFunc("/", index)
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+    url := "127.0.0.1:8080"
+    log.Print("listening on ", url)
+    log.Fatal(http.ListenAndServe(url, nil))
+}
